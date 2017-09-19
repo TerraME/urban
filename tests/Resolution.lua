c79f50f8ce105c37003a18777ef683f101a75d7f
washington-PC\washington <wsenfranca@gmail.com>
2017-09-19 12:11:38 -0300

    Adding Resolution model.

--- tests/Resolution.lua
+++ tests/Resolution.lua
@@ -5,7 +5,7 @@ return{
 	Resolution = function(unitTest)
 		local model = Resolution{finalTime = 5, strategy = decideBoth, quantity = 300, cityDimension = 15}
 		model:run()
-		unitTest:assertSnapshot(model.mapHistogram, "ResolutionHistogram.png", 0.05)
-		unitTest:assertSnapshot(model.mapTotalHistogram, "ResolutionTotalHistogram.png", 0.05)
+		unitTest:assertSnapshot(model.mapHistogram, "ResolutionHistogram.png", 0.15)
+		unitTest:assertSnapshot(model.mapTotalHistogram, "ResolutionTotalHistogram.png", 0.15)
 	end,
 }
